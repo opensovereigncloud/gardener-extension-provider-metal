92c526d7cd082b0d4c387cbcc21790bf503e692f
Nuckal777 <erik.schubert@sap.com>
2025-01-27 13:46:02 +0100

    Fix doc rename

--- pkg/apis/metal/v1alpha1/doc.go
+++ pkg/apis/metal/v1alpha1/doc.go
@@ -7,5 +7,5 @@
 // +k8s:defaulter-gen=TypeMeta
 
 // Package v1alpha1 contains the metal provider API resources.
-// +groupName=metal.provider.extensions.gardener.cloud
+// +groupName=ironcore-metal.provider.extensions.gardener.cloud
 package v1alpha1 // import "github.com/ironcore-dev/gardener-extension-provider-ironcore-metal/pkg/apis/metal/v1alpha1"
