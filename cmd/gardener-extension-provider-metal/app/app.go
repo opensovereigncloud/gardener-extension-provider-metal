6e5e7edd033340bd3b25261fa4d996647f368d0d
Sujeet <phadtare.sujeet@gmail.com>
2025-06-04 14:21:16 +0530

    Update `gardener/gardener` to `v1.118.0` (#191)
    * Update `gardener/gardener` to `v1.118.0`
    
    * Adapt code to `gardener/gardener` `v1.118.0`
    
    * Register shoot webhooks earlier in case of autonomous shoot clusters
    
    * Do not watch `Machine`s in Worker controller in autonomous shoot clusters
    
    * Run `make generate`

--- cmd/gardener-extension-provider-metal/app/app.go
+++ cmd/gardener-extension-provider-metal/app/app.go
@@ -191,8 +191,9 @@ func NewControllerManagerCommand(ctx context.Context) *cobra.Command {
 			reconcileOpts.Completed().Apply(&infrastructurecontroller.DefaultAddOptions.IgnoreOperationAnnotation, &infrastructurecontroller.DefaultAddOptions.ExtensionClass)
 			reconcileOpts.Completed().Apply(&workercontroller.DefaultAddOptions.IgnoreOperationAnnotation, &workercontroller.DefaultAddOptions.ExtensionClass)
 			workercontroller.DefaultAddOptions.GardenCluster = gardenCluster
+			workercontroller.DefaultAddOptions.AutonomousShootCluster = generalOpts.Completed().AutonomousShootCluster
 
-			if _, err := webhookOptions.Completed().AddToManager(ctx, mgr, nil); err != nil {
+			if _, err := webhookOptions.Completed().AddToManager(ctx, mgr, nil, generalOpts.Completed().AutonomousShootCluster); err != nil {
 				return fmt.Errorf("could not add webhooks to manager: %w", err)
 			}
 			metalcontrolplane.DefaultAddOptions.WebhookServerNamespace = webhookOptions.Server.Namespace
